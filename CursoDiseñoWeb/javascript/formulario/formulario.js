ac22a30d975a4db75096e4321623fadabf2de0a7
soycolgado <soycolgado>
2018-09-01 20:34:06 -0300

    seguimiento de practicas js

--- CursoDiseñoWeb/javascript/formulario/formulario.js
+++ CursoDiseñoWeb/javascript/formulario/formulario.js
@@ -1,1 +1,16 @@
-var formulario = document.getElementById('formulario');
+var formulario = document.getElementById('formulario'),
+    nombre = formulario.nombre;
+    correo = formulario.correo;
+    sexo = formulario.sexo;
+    terminos = formulario.terminos,
+    error = document.getElementById('error');
+
+    function validarNombre(e){
+        if(nombre.value == "" || nombre.value == null){
+            error.style.display = "block";
+            error.innerHTML += '<li>Por favor completa el nombre</li>';
+            e.preventDefault();
+        }
+    }
+
+    formulario.addEventListener('submit',validarNombre);
