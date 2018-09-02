796a0bf6c1564674fc0203f5b4abc53689c7525f
soycolgado <soycolgado>
2018-09-02 01:28:51 -0300

    Final 1ra practica javascript

--- CursoDiseñoWeb/javascript/formulario/formulario.js
+++ CursoDiseñoWeb/javascript/formulario/formulario.js
@@ -10,7 +10,47 @@ var formulario = document.getElementById('formulario'),
             error.style.display = "block";
             error.innerHTML += '<li>Por favor completa el nombre</li>';
             e.preventDefault();
+        }else{
+            error.style.display = "none";
         }
     }
 
-    formulario.addEventListener('submit',validarNombre);
+    function validarCorreo(e){
+        if(correo.value == "" || correo.value==null){
+            error.style.display = "block";
+            error.innerHTML += "<li>Por favor completa el correo</li>";
+            e.preventDefault();
+        }else{
+            error.style.display = "none";
+        }
+    }
+
+    function validarSexo(e){
+        if(sexo.value == "" || sexo.value == null){
+            error.style.display = "block";
+            error.innerHTML += "<li>Por favor completa el sexo</li>";
+            e.preventDefault();
+        }else{
+            error.style.display="none";
+        }
+    }
+
+    function validarTerminos(e){
+        if(!terminos.checked){
+            error.style.display = "block";
+            error.innerHTML+="<li>Por favor acepte los terminos y condiciones</li>"
+            e.preventDefault();
+        }else{
+            error.style.display="none";
+        }
+    }
+
+    function validarFormulario(e){
+        error.innerHTML = "";
+        validarNombre(e);
+        validarCorreo(e);
+        validarSexo(e);
+        validarTerminos(e);
+    }
+
+    formulario.addEventListener('submit',validarFormulario);
