2c7bdb244a2501490c49ce6a614a5912cd1b5915
soycolgado <soycolgado>
2018-09-07 22:57:58 -0300

    Jquery

---
+++ CursoDiseñoWeb/jquery/efectos.js
@@ -0,0 +1,17 @@
+//$(document).ready(function(){
+    // function efecto(){
+    //     alert();
+    // }
+
+    function efecto(){
+        //$(selector).efecto(duracion,callback);
+        // $('.caja').hide(1000,function(){
+        //     alert('desaparecio');
+        // });
+        // $('.caja').toggle(1000);
+        // $('.caja').fadeIn(1000);
+        // $('.caja').fadeOut(1000);
+        // $('.caja').fadeToggle(1000);
+        $('.caja').slideToggle(1000);
+    }
+//});
---
+++ CursoDiseñoWeb/jquery/selectores.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    // $('h1, h2').hide();
+    // $('.encabezado').hide();
+    // $('h1').hide();
+    // $('#primero').hide();
+    // $('h1.encabezado').hide();
+    $('.primero a').hide();
+});
