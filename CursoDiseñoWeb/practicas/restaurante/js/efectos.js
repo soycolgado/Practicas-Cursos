06eaebcb7f8c413137d29805fdab5c3b63442c17
soycolgado <soycolgado>
2018-09-30 20:01:46 -0300

    Fin pagina restaurante

--- CursoDiseñoWeb/practicas/restaurante/js/efectos.js
+++ CursoDiseñoWeb/practicas/restaurante/js/efectos.js
@@ -1,53 +1,60 @@
-$(document).ready(function(){
-    $('.menu a').each(function(index,valorElemento){
+$(document).ready(function () {
+    $('.menu a').each(function (index, valorElemento) {
         $(this).css({
-            'top':'-200px'
+            'top': '-200px'
         });
 
         $(this).animate({
-            'top':'0px'
-        },2000 + (index*500));
+            'top': '0px'
+        }, 2000 + (index * 500));
     });
-    
 
-    if( $(window).width() > 800){
+
+    if ($(window).width() > 800) {
         $('header .textos').css({
-            opacity:'0',
-            marginTop:'0px'
+            opacity: '0',
+            marginTop: '0px'
         });
 
         $('header .textos').animate({
-            opacity:'1',
-            marginTop:'-52px'
-        },1500);
+            opacity: '1',
+            marginTop: '-52px'
+        }, 1500);
     }
 
     var acercaDe = $('#acerca-de').offset().top,
-        menu = $('#platillos').offset().top,    
+        menu = $('#platillos').offset().top,
         galeria = $('#galeria').offset().top,
         ubicacion = $('#ubicacion').offset().top;
 
-    $('#btn-acerca-de').on('click',function(){
+    $('#btn-acerca-de').on('click', function (e) {
+        e.preventDefault();
         $('html, body').animate({
-            scrollTop:acercaDe -100
-        },500);
+            scrollTop: acercaDe - 100
+        }, 500);
     });
 
-    $('#btn-menu').on('click',function(){
+    $('#btn-menu').on('click', function (e) {
+        e.preventDefault();
+
         $('html, body').animate({
-            scrollTop:menu
-        },500);
+            scrollTop: menu
+        }, 500);
     });
 
-    $('#btn-galeria').on('click',function(){
+    $('#btn-galeria').on('click', function (e) {
+        e.preventDefault();
+
         $('html, body').animate({
-            scrollTop:galeria
-        },500);
+            scrollTop: galeria
+        }, 500);
     });
 
-    $('#btn-ubicacion').on('click',function(){
+    $('#btn-ubicacion').on('click', function (e) {
+        e.preventDefault();
+
         $('html, body').animate({
-            scrollTop:ubicacion
-        },500);
+            scrollTop: ubicacion
+        }, 500);
     });
 });
