bb309ce8b7ab7478ca56ce7cc455fd32112d35f4
soycolgado <soycolgado>
2018-09-30 14:22:56 -0300

    Efectos jquery Terminados

---
+++ CursoDiseñoWeb/practicas/restaurante/js/efectos.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+    $('.menu a').each(function(index,valorElemento){
+        $(this).css({
+            'top':'-200px'
+        });
+
+        $(this).animate({
+            'top':'0px'
+        },2000 + (index*500));
+    });
+    
+
+    if( $(window).width() > 800){
+        $('header .textos').css({
+            opacity:'0',
+            marginTop:'0px'
+        });
+
+        $('header .textos').animate({
+            opacity:'1',
+            marginTop:'-52px'
+        },1500);
+    }
+
+    var acercaDe = $('#acerca-de').offset().top,
+        menu = $('#platillos').offset().top,    
+        galeria = $('#galeria').offset().top,
+        ubicacion = $('#ubicacion').offset().top;
+
+    $('#btn-acerca-de').on('click',function(){
+        $('html, body').animate({
+            scrollTop:acercaDe -100
+        },500);
+    });
+
+    $('#btn-menu').on('click',function(){
+        $('html, body').animate({
+            scrollTop:menu
+        },500);
+    });
+
+    $('#btn-galeria').on('click',function(){
+        $('html, body').animate({
+            scrollTop:galeria
+        },500);
+    });
+
+    $('#btn-ubicacion').on('click',function(){
+        $('html, body').animate({
+            scrollTop:ubicacion
+        },500);
+    });
+});
