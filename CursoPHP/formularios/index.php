c5e8c8ef783ac999822a774f7d629dfffb09e181
soycolgado <39932051+soycolgado@users.noreply.github.com>
2019-03-05 15:18:39 -0300

    ASD

---
+++ CursoPHP/formularios/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="recibe.php" method="get">
+        <input type="text" name="nombre" placeholder="Nombre: " id="">
+        <br>
+        <label for="hombre">Hombre</label>
+        <input type="radio" name="sexo" value="hombre" id="hombre">
+        <br>
+        <label for="mujer">Mujer</label>
+        <input type="radio" name="sexo" id="mujer" value="mujer">
+        <br>
+        <select name="year" id="year">
+            <option value="2000">2000</option>
+            <option value="2001">2001</option>
+            <option value="2003">2003</option>
+        </select>
+        <br>
+        <label for="terminos">Terminos y condiciones</label>
+        <input type="checkbox" name="terminos" id="terminos">
+        <br>
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
---
+++ CursoPHP/formularios/recibe.php
@@ -0,0 +1,13 @@
+<?php
+// if($_POST){
+
+//     $nombre = $_POST['nombre'];
+//     $sexo = $_POST['sexo'];
+//     $year = $_POST['year'];
+//     $terminos = $_POST['terminos'];
+    
+//     echo 'Hola, '.$nombre.' eres '.$sexo;
+// }else{
+//     header('Location: http://www.google.com');
+// }
+?>
