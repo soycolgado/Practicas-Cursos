7f40c5ba5091d5d026780bd159e2104cff954f9a
soycolgado <39932051+soycolgado@users.noreply.github.com>
2019-03-07 15:20:00 -0300

    asd

--- CursoPHP/practicas/formulario_contacto/index.php
+++ CursoPHP/practicas/formulario_contacto/index.php
@@ -1,3 +1,29 @@
 <?php
+$errores = "";
+$enviado = "";
+
+if(isset($_POST['submit'])){
+    $nombre = $_POST['nombre'];
+    $correo = $_POST['correo'];
+    $mensaje = $_POST['mensaje'];
+
+    if(!empty($nombre)){
+        $nombre = trim($nombre);
+        $nombre = filter_var($nombre,FILTER_SANITIZE_STRING);
+    }else{
+        $errores .= "Por favor ingresa un Nombre<br>";
+    }
+
+    if(!empty($correo)){
+        $correo = filter_var($correo,FILTER_SANITIZE_EMAIL);
+
+        if(!filter_var($correo,FILTER_VALIDATE_EMAIL)){
+            $errores .= "Por favor ingrese un Mail valido <br>";
+        }
+    }else{
+        $errores .= "Por favor ingrese un Mail <br>";
+    }
+}
+
 require('index.view.php');
 ?>
--- CursoPHP/practicas/formulario_contacto/index.view.php
+++ CursoPHP/practicas/formulario_contacto/index.view.php
@@ -15,6 +15,15 @@
             <input type="text" name="nombre" placeholder="Nombre" value="" id="nombre" class="form-control">
             <input type="text" name="correo" id="correo" placeholder="Correo:" value="" class="form-control">
             <textarea name="mensaje" id="mensaje" class="form-control" placeholder="Mensaje"></textarea>
+            <?php if(!empty($errores)):?>
+            <div class="alert error" role="alert">
+                <?php echo $errores;?>
+            <div>
+            <?php elseif($enviado):?>
+            <div class="alert success" role="alert">
+                <?php echo "Enviado correctamente";?>
+            </div>
+<?php endif;?>
             <input type="submit" value="Enviar correo" name="submit" id="submit" class="btn btn-primary">
         </form>
     </div>
