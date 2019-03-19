0200d0e35932e1efabc6616fe222fdcc7a3b3d01
soycolgado <39932051+soycolgado@users.noreply.github.com>
2019-03-19 14:25:59 -0300

    a

---
+++ CursoPHP/practicas/paginacion/index.view.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Paginacion</title>
+</head>
+<body>
+    <div class="contenedor">
+        <h1>Articulos</h1>
+        <section class="articulos">
+            <ul>
+                <li>1 - Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam, incidunt.</li>
+                <li>2 - Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam, incidunt.</li>
+                <li>3 - Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam, incidunt.</li>
+                <li>4 - Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam, incidunt.</li>
+                
+            </ul>
+        </section>
+        <section>
+        <ul>
+            <li><a href="#">&laquo</a></li>
+            <li><a href="#">1</a></li>
+            <li><a href="#">2</a></li>
+            <li><a href="#">3</a></li>
+            <li><a href="#">4</a></li>
+            <li><a href="#">&raquo</a></li>
+        </ul>
+        </section>
+    </div>
+</body>
+</html>
