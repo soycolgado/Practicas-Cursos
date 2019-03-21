7aa8c77e21068e6ba50384baf8d1a601ce773e0b
soycolgado <39932051+soycolgado@users.noreply.github.com>
2019-03-21 15:39:18 -0300

    1

--- CursoPHP/practicas/paginacion/index.php
+++ CursoPHP/practicas/paginacion/index.php
@@ -1,25 +1,10 @@
 <?php
-   try{
-        $conexion = new PDO('mysql:host=localhost; dbname=paginacion_pagina','root','');
-   }catch(PDOException $e){
-       echo "Error: ".$e->getMessage();
-   }
 
-   $pagina = isset($_GET['pagina']) ? (int)$_GET['pagina'] : 1;
-   $postPorPagina = 5;
-   $inicio = ($pagina > 1) ? ($pagina * $postPorPagina - $postPorPagina) : 0;
-   $articulos = $conexion->prepare("SELECT SQL_CALC_FOUND_ROWS * FROM articulos LIMIT $inicio,$postPorPagina");
-   $articulos->execute();
-   $articulos = $articulos->fetchAll();
-
-   if(!$articulos){
-       header('Location: index.php');
-   }
-
-   $totalArticulos = $conexion->query('SELECT FOUND_ROWS AS total');
-   $totalArticulos = $totalArticulos->fetch()['total'];
-
-   $numeroPaginas = ceil($totalArticulso / $postPorPagina);
-
-    require "index.view.php";
+    try {
+        //code...
+    } catch (PDOException $e) {
+        echo "Error: ".$e->getMessage();
+        die();
+    }
+   require 'index.view.php';
 ?>
