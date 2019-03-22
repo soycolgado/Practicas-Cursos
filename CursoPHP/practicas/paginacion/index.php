7911219e5a4c1dcc51cb9d36df17db2489783402
soycolgado <39932051+soycolgado@users.noreply.github.com>
2019-03-22 00:36:51 -0300

    1

--- CursoPHP/practicas/paginacion/index.php
+++ CursoPHP/practicas/paginacion/index.php
@@ -1,10 +1,31 @@
 <?php
+try{
+    $conexion = new PDO('mysql:host=localhost;dbname=paginacion_practica', 'root','');
+}catch(PDOException $e){
+    echo "Error: ".$e->getMessage();
+    die();
+}
 
-    try {
-        //code...
-    } catch (PDOException $e) {
-        echo "Error: ".$e->getMessage();
-        die();
-    }
-   require 'index.view.php';
+$pagina = isset($_GET['pagina']) ? (int)$_GET['pagina'] : 1;
+$postPorPagina = 5;
+
+$inicio = ($pagina > 1) ? ($pagina * $postPorPagina - $postPorPagina) : 0;
+
+$articulos = $conexion->prepare("SELECT SQL_CALC_FOUND_ROWS * FROM articulos LIMIT $inicio,$postPorPagina");
+
+$articulos->execute();
+$articulos = $articulos->fetchAll();
+
+if(!$articulos){
+    header('Location:index.php');
+}
+
+$totalArticulos = $conexion->prepare("SELECT FOUND_ROWS() as total");
+$totalArticulos->execute();
+$totalArticulos = $totalArticulos->fetch()['total'];
+
+$numeroPagina = ceil($totalArticulos / $postPorPagina);
+
+require 'index.view.php';
+    
 ?>
--- CursoPHP/practicas/paginacion/index.view.php
+++ CursoPHP/practicas/paginacion/index.view.php
@@ -12,21 +12,41 @@
         <h1>Articulos</h1>
         <section class="articulos">
             <ul>
-                <li>1 - Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam, incidunt.</li>
-                <li>2 - Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam, incidunt.</li>
-                <li>3 - Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam, incidunt.</li>
-                <li>4 - Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam, incidunt.</li>
-                
+                <?php foreach($articulos as $articulo):?>
+                     <li><?php echo $articulo['id'].'-'.$articulo['articulo']?></li>           
+
+                <?php endforeach;?>
             </ul>
         </section>
         <section class="paginacion">
         <ul>
-            <li class="disable">&laquo</li>
+            <?php if($pagina == 1):?>
+                <li class="disable">&laquo;</li>
+            <?php else:?>
+                <li><a href="?pagina=<?php echo $pagina - 1?>">&laquo;</a></li>
+            <?php endif;?>
+            
+            <?php
+                for($i = 1; $i <= $numeroPagina; $i++){
+                    if($pagina == $i){
+                        echo "<li class='active'><a href='?pagina=$i'>$i</a></li>";
+                    }else{
+                        echo "<li><a href='?pagina=$i'>$i</a></li>";
+                    }
+                }
+            ?>
+
+            <?php if($pagina == $numeroPagina):?>
+                <li class="disable">&raquo;</li>
+            <?php else:?>
+                <li><a href="?pagina=<?php echo $pagina + 1?>">&raquo;</a></li>
+            <?php endif;?>
+            <!-- <li class="disable">&laquo</li>
             <li class="active"><a href="#">1</a></li>
             <li><a href="#">2</a></li>
             <li><a href="#">3</a></li>
             <li><a href="#">4</a></li>
-            <li><a href="#">&raquo</a></li>
+            <li><a href="#">&raquo</a></li> -->
         </ul>
         </section>
     </div>
