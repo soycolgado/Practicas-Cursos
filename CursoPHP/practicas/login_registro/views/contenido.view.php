ac92de82393a9457da67169a9b49415e2bce3e59
soycolgado <39932051+soycolgado@users.noreply.github.com>
2019-04-02 18:19:42 -0300

    c

---
+++ CursoPHP/practicas/login_registro/views/contenido.view.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/estilos.css">
+    <title>Contenido</title>
+    <div class="contenedor">
+        <h1 class="titulo">Contenido</h1>
+        <a href="cerrar.php">Cerrar sesion</a>
+        <hr class="border">
+        <div class="contenido">
+        <article>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, sed placeat aliquam ut mollitia quas repellat! Perferendis a, ex, laboriosam alias, delectus quidem unde deserunt excepturi vero eaque sed natus.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem laborum suscipit, modi expedita voluptate similique esse sed non quibusdam nisi? Sit cumque libero, accusamus deleniti sed enim voluptate perferendis illo.</p>
+        </article>
+        </div>
+    </div>
+</head>
+<body>
+    
+</body>
+</html>
--- CursoPHP/practicas/paginacion/index.php
+++ CursoPHP/practicas/paginacion/index.php
@@ -1,31 +1,25 @@
 <?php
-try{
-    $conexion = new PDO('mysql:host=localhost;dbname=paginacion_practica', 'root','');
-}catch(PDOException $e){
-    echo "Error: ".$e->getMessage();
-    die();
-}
+   try{
+        $conexion = new PDO('mysql:host=localhost; dbname=paginacion_pagina','root','');
+   }catch(PDOException $e){
+       echo "Error: ".$e->getMessage();
+   }
 
-$pagina = isset($_GET['pagina']) ? (int)$_GET['pagina'] : 1;
-$postPorPagina = 5;
+   $pagina = isset($_GET['pagina']) ? (int)$_GET['pagina'] : 1;
+   $postPorPagina = 5;
+   $inicio = ($pagina > 1) ? ($pagina * $postPorPagina - $postPorPagina) : 0;
+   $articulos = $conexion->prepare("SELECT SQL_CALC_FOUND_ROWS * FROM articulos LIMIT $inicio,$postPorPagina");
+   $articulos->execute();
+   $articulos = $articulos->fetchAll();
 
-$inicio = ($pagina > 1) ? ($pagina * $postPorPagina - $postPorPagina) : 0;
+   if(!$articulos){
+       header('Location: index.php');
+   }
 
-$articulos = $conexion->prepare("SELECT SQL_CALC_FOUND_ROWS * FROM articulos LIMIT $inicio,$postPorPagina");
+   $totalArticulos = $conexion->query('SELECT FOUND_ROWS AS total');
+   $totalArticulos = $totalArticulos->fetch()['total'];
 
-$articulos->execute();
-$articulos = $articulos->fetchAll();
+   $numeroPaginas = ceil($totalArticulso / $postPorPagina); 
 
-if(!$articulos){
-    header('Location:index.php');
-}
-
-$totalArticulos = $conexion->prepare("SELECT FOUND_ROWS() as total");
-$totalArticulos->execute();
-$totalArticulos = $totalArticulos->fetch()['total'];
-
-$numeroPagina = ceil($totalArticulos / $postPorPagina);
-
-require 'index.view.php';
-    
+    require "index.view.php";
 ?>
