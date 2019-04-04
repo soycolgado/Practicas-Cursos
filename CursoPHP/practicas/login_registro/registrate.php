ebbcf7e526b85a1c95ab50854f8258515df460f2
soycolgado <39932051+soycolgado@users.noreply.github.com>
2019-04-03 23:02:56 -0300

    c

--- CursoPHP/practicas/login_registro/registrate.php
+++ CursoPHP/practicas/login_registro/registrate.php
@@ -31,6 +31,20 @@
             if($resultado != false){
                 $errores.= '<li>El nombre de usuario ya existe</li>';
             }
+
+            //Hasheo de contraseña
+            $password = hash('sha512',$password);
+            $password2 = hash('sha512',$password2);
+
+            if($password != $password2){
+                $errores .= '<li>Las contraseñas no son iguales</li>';
+            }
+        }
+
+        if($errores == ''){
+            $statement = $conexion->prepare("INSERT INTO usuarios (id,usuario,pass) VALUES(null,:usuario,:pass)");
+            $statement->execute(array(':usuario' => $usuario, ':pass' => $password));
+            header('Location: login.php');
         }
     }
     require 'views/registrate.view.php';
