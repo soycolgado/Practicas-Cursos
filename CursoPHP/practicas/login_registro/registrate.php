6d108421ff47bedca84ecb4b1da64e54c27fd2b7
soycolgado <39932051+soycolgado@users.noreply.github.com>
2019-04-08 21:28:21 -0300

    Update registrate.php

--- CursoPHP/practicas/login_registro/registrate.php
+++ CursoPHP/practicas/login_registro/registrate.php
@@ -1,3 +1,32 @@
 <?php
+    if(isset($_SESSION['usuario'])){
+        header('location: index.php');
+    }
+
+    if($_SERVER['REQUEST_METHOD']== 'POST'){
+        $usuario = strtolower($_POST['usuario']);
+        $usuario = filter_var($usuario,FILTER_SANITIZE_STRING);
+        $password = $_POST['password'];
+        $password2 = $_POST['password2'];        
+    }
+    echo $usuario;
+    $errores ='';
+
+    if(empty($usuario) || empty($password) || empty($password2)){
+        $errores.='<li> Por favor rellena los campos correctamente</li>';
+    }else{
+        try {
+            $conexion = new PDO('mysql:host=localhost; dbname=login_practica','root','');
+        } catch (PDOException $e) {
+            echo 'Error: '.$e->getMessage();
+        }
+
+        $statement = $conexion->prepare("SELECT * FROM usuarios WHERE usuario = :usuario LIMIT 1");
+        $statement->execute(array(':usuario'=>$usuario));
+        $resultado = $statement->fetchAll();
+        if($resultado != false){
+            $errores.='<li>El usuario ya existe</li>'; 
+        }
+    }
     require 'views/registrate.view.php';
 ?>
