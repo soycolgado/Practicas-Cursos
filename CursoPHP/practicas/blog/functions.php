dd6e1245f0dfea2ce6a8918c2a0c6a9cc7254826
soycolgado <39932051+soycolgado@users.noreply.github.com>
2019-04-20 23:35:02 -0300

    c

--- CursoPHP/practicas/blog/functions.php
+++ CursoPHP/practicas/blog/functions.php
@@ -1,7 +1,8 @@
 <?php
+    // require 'admin/config.php';
     function conexion ($db_config){
         try{
-            $conexion = new PDO("mysql:host=localhost;dbname=blog_practica","root","");
+            $conexion = new PDO("mysql:host=localhost;dbname=".$db_config['basedatos'],$db_config['usuario'],$db_config['pass']);
             return $conexion;
         }catch(PDOException $e){
             return false;
@@ -13,6 +14,14 @@
         $datos = stripslashes($datos);
         $datos = htmlspecialchars($datos);
         return $datos;
+    }
+
+    function pagina_actual(){
+        return isset($_GET['p']) ? $_GET['p'] : 1;
+    }
+
+    function obtener_post($post_por_pagina,$conexion){
+        $inicio = (pagina_actual() > 1) ? pagina_actual() * $post_por_pagina - $post_por_pagina : 0;
     }
 
 ?>
--- CursoPHP/practicas/blog/index.php
+++ CursoPHP/practicas/blog/index.php
@@ -1,4 +1,12 @@
 <?php
 require 'admin/config.php';
+require 'functions.php';
+
+$conexion = conexion($db_config);
+if(!$conexion){
+    header('Location: error.php');
+}
+
+// obtener_post();
 require 'views/index.view.php';
 ?>
