f76df45eafbf60386d2be966a758338540648f5b
soycolgado <39932051+soycolgado@users.noreply.github.com>
2019-04-29 02:08:24 -0300

    c

---
+++ practica_propias/views/modules/navegacion.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+        <li><a href="">Inicio</a></li>
+        <li><a href="">Nosotros</a></li>
+        <li><a href="">Servicios</a></li>
+        <li><a href="">Contactenos</a></li>
+    </ul>
+</nav>
