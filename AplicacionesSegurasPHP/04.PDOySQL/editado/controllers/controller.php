22382e40d3b777bd66cc19b32605a11d98f652bc
soycolgado <39932051+soycolgado@users.noreply.github.com>
2019-04-30 15:52:02 -0300

    c

---
+++ AplicacionesSegurasPHP/04.PDOySQL/editado/controllers/controller.php
@@ -0,0 +1,54 @@
+<?php
+
+class MvcController{
+
+	#LLAMADA A LA PLANTILLA
+	#-------------------------------------
+
+	public function pagina(){	
+		
+		include "views/template.php";
+	
+	}
+
+	#ENLACES
+	#-------------------------------------
+
+	public function enlacesPaginasController(){
+
+		if(isset( $_GET['action'])){
+			
+			$enlaces = $_GET['action'];
+		
+		}
+
+		else{
+
+			$enlaces = "index";
+		}
+
+		$respuesta = Paginas::enlacesPaginasModel($enlaces);
+
+		include $respuesta;
+
+	}
+
+	#REGISTRO DE USUARIOS
+	public function registroUsuarioController(){
+		if(isset($_POST['usuario'])){
+
+			$datosController = array("usuario" => $_POST['usuario'], "password" => $_POST['password'], "email" => $_POST['email']);
+			
+			$respuesta = Datos::registroUsuarioModel($datosController,"usuarios");
+			
+			if($respuesta == "Success"){
+				header("Location: index.php?action=ok");
+			}else{
+				header("Location:index.php");
+			}
+		}
+	}
+
+}
+
+?>
---
+++ AplicacionesSegurasPHP/04.PDOySQL/editado/views/modules/registro.php
@@ -0,0 +1,24 @@
+<h1>REGISTRO DE USUARIO</h1>
+
+<form method="post" action="">
+	
+	<input type="text" placeholder="Usuario" name="usuario" required>
+
+	<input type="password" placeholder="Contraseña" name="password" required>
+
+	<input type="email" placeholder="Email" name="email" required>
+
+	<input type="submit" value="Enviar">
+
+</form>
+
+<!-- <?php
+	$registro = new MvcController();
+	$registro -> registroUsuarioController();
+
+	if(isset($_GET['action'])){
+		if($_GET['action'] == 'ok'){
+			echo "Registro Exitoso";
+		}
+	}
+?> -->
