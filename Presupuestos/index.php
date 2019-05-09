1208111a77dea6270e968596631878818d1153a7
soycolgado <39932051+soycolgado@users.noreply.github.com>
2019-05-09 09:48:55 -0300

    c

--- Presupuestos/index.php
+++ Presupuestos/index.php
@@ -25,9 +25,10 @@
     //Inicializacion de una variable que va a contener el nombre de la tabla
     
     //Ciclo que recorre las filas
-    for ($row = 21; $row <= $highestRow; $row++) {
+    for ($row = 2; $row <= $highestRow; $row++) {
         $tabla = "";
-        $precioMasBajo = 9999; //Inicializacion
+        $precioMasBajo = array("precio" => 9999,"distri"=>"");
+        // $precioMasBajo = 9999; //Inicializacion
         //Tomo la fila B que es la descripcion para poder comparar su valor
         $descripcion = strtolower($sheet->getCell("B".$row)->getValue());
         $tipo = strtolower($sheet->getCell("C".$row)->getValue());
@@ -36,8 +37,6 @@
             $tabla = "procesador";
         } elseif (preg_match("/mother+/", $descripcion) && ($sku != "SKU")) {
             $tabla = "mother";
-            echo $descripcion;
-            echo "<br>";
         } elseif (preg_match("/memoria/", $descripcion) && ((preg_match("/ddr+/", $tipo)) || (preg_match("/sodimm/", $tipo)))) {
             $tabla = "memoria";
         }elseif(preg_match("/d..co+/",$descripcion) && (($tipo == "ssd") || (preg_match("/notebook+/",$tipo)) || (preg_match("/pc/",$tipo)) )){
@@ -48,21 +47,21 @@
             // $sku = $sheet->getCell("A" . $row)->getValue();
             $marca = $sheet->getCell("D" . $row)->getValue();
             for ($col = "F"; $col != $highestColumn; $col++) {
-    
+                $c = $sheet->getCell($col."1")->getValue();
+                // echo $c;
                 $precio = $sheet->getCell($col . $row)->getValue();
-                $precioMasBajo = Comparar::compararPrecio($precio, $precioMasBajo,$col);
+                $precioMasBajo = Comparar::compararPrecio($precio, $precioMasBajo,$c);
                 // $precio = (int)$precio;
                 // if($precio != null && $precio < $precioMasBajo){
                 //     $precioMasBajo = $precio;
                 // }
-
+                
 
                 //Llamada al metodo de carga
             }
-        echo $precioMasBajo["precio"];
-        echo "<br>";
-        echo $precioMasBajo["columna"];
-        // CargarLibro::cargar($tabla,$sku, $descripcion, $marca);
+            // echo "$sku - $descripcion - $marca - ".$precioMasBajo['precio']." - ".$precioMasBajo['distri'];
+            // echo "<br>";
+        CargarLibro::cargar($tabla,$sku, $descripcion, $marca,$precioMasBajo['precio'],$precioMasBajo['distri']);
         }
     }
     ?>
--- Presupuestos/model/comparar.php
+++ Presupuestos/model/comparar.php
@@ -2,14 +2,12 @@
     class Comparar{
         public static function compararPrecio($precio,$precioMasBajo,$c){
             $precio = (float)$precio;
-            $precioColumna = array("precio" => $precioMasBajo,"columna" => $c);
-            if($precio != null && $precio < $precioMasBajo){
-                $precioMasBajo = $precio;
-                $col = $c;
-                $precioColumna["precio"] = $precioMasBajo;
-                $precioColumna["columna"] = $col;
+            // $precioColumna = array("precio" => $precioMasBajo, "distri" => $c);
+            if($precio != null && $precio < $precioMasBajo["precio"]){
+                $precioMasBajo["precio"] = $precio;
+                $precioMasBajo["distri"] = $c;
             }
-            return $precioColumna;
+            return $precioMasBajo;
         }
     }
     
