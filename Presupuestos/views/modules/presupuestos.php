a0ebbbe9ab0ff8e188dca2ac9f1a65ceb35a37d0
soycolgado <39932051+soycolgado@users.noreply.github.com>
2019-05-14 22:51:43 -0300

    c

--- Presupuestos/views/modules/presupuestos.php
+++ Presupuestos/views/modules/presupuestos.php
@@ -1,39 +1,124 @@
-<div class="img-group">
-    <div class="thumb">
-        <a href="#"><img src="recursos/procesador.jpg" alt=""></a>
+<article class="contenido-izquierda">
+
+    
+    <div class="img-group">
+        <div class="componente">
+            
+            <div class="thumb">
+                <a href="#"><img src="recursos/procesador.jpg" alt=""></a>
+            </div>
+            <div class="descripcion">
+                <p>
+                    Seleccione el Procesador
+                </p>
+            </div>
+            
+        </div>
+        
+        <div class="componente">
+            
+            <div class="thumb">
+                <a href="#"><img src="recursos/mother.jpg" alt=""></a>
+            </div>
+            <div class="descripcion">
+                
+                <p>
+                    Seleccione el Mother
+                </p>
+            </div>
+        </div>
+        
     </div>
-    <div class="thumb">
-        <a href="#"><img src="recursos/mother.jpg" alt=""></a>
+    
+    
+    <div class="img-group">
+        <div class="componente">
+            
+            <div class="thumb">
+                <a href="#"><img src="recursos/memoria.jpg" alt=""></a>
+            </div>
+            <div class="descripcion">
+                <p>
+                    Seleccione la Memoria
+                </p>
+            </div>
+            
+        </div>
+        
+        <div class="componente">
+            
+            <div class="thumb">
+                <a href="#"><img src="recursos/discoHD.jpg" alt=""></a>
+            </div>
+            <div class="descripcion">
+                
+                <p>
+                    Seleccione el Disco Mecanico
+                </p>
+            </div>
+        </div>
+        
     </div>
-
-</div>
-
-<div class="img-group">
-
-
-    <div class="thumb">
-        <a href="#"><img src="recursos/memoria.jpg" alt=""></a>
+    
+    <div class="img-group">
+        <div class="componente">
+            
+            <div class="thumb">
+                <a href="#"><img src="recursos/discoSSD.jpg" alt=""></a>
+            </div>
+            <div class="descripcion">
+                <p>
+                    Seleccione el Disco SSD
+                </p>
+            </div>
+            
+        </div>
+        
+        <div class="componente">
+            
+            <div class="thumb">
+                <a href="#"><img src="recursos/vga.jpg" alt=""></a>
+            </div>
+            <div class="descripcion">
+                
+                <p>
+                    Seleccione la Placa de Video
+                </p>
+            </div>
+        </div>
+        
     </div>
-    <div class="thumb">
-        <a href="#"><img src="recursos/discoHD.jpg" alt=""></a>
+    
+    <div class="img-group">
+        <div class="componente">
+            
+            <div class="thumb">
+                <a href="#"><img src="recursos/fuente.jpg" alt=""></a>
+            </div>
+            <div class="descripcion">
+                <p>
+                    Seleccione la Fuente
+                </p>
+            </div>
+            
+        </div>
+        
+        <div class="componente">
+            
+            <div class="thumb">
+                <a href="#"><img src="recursos/gabinete.jpg" alt=""></a>
+            </div>
+            <div class="descripcion">
+                
+                <p>
+                    Seleccione el Gabinete
+                </p>
+            </div>
+        </div>
+        
     </div>
-</div>
+</article>
 
-<div class="img-group">
-<div class="thumb">
-    <a href="#"><img src="recursos/discoSSD.jpg" alt=""></a>
-</div>
-<div class="thumb">
-    <a href="#"><img src="recursos/vga.jpg" alt=""></a>
-</div>
-</div>
-
-
-<div class="img-group">
-<div class="thumb">
-    <a href="#"><img src="recursos/fuente.jpg" alt=""></a>
-</div>
-<div class="thumb">
-    <a href="#"><img src="recursos/gabinete.jpg" alt=""></a>
-</div>
-</div>
+<article class="contenido-derecha">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et, autem repellat tenetur sequi consequuntur mollitia quam distinctio quisquam nisi obcaecati, reiciendis at facilis ad neque. Aspernatur eligendi labore iusto accusamus!
+</article>
