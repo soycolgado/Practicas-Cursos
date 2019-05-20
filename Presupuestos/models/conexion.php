7fd95ba2ac249719c9c2408bb2661d5ed9400918
soycolgado <39932051+soycolgado@users.noreply.github.com>
2019-05-20 19:14:57 -0300

    c

--- Presupuestos/models/conexion.php
+++ Presupuestos/models/conexion.php
@@ -2,7 +2,7 @@
     class Conexion{
         public static function conectar(){
             try{
-                $link = new PDO("mysql:host=localhost;dbname=Pruebas","root","");
+                $link = new PDO("mysql:host=localhost;dbname=Pruebas","root","root");
                 return $link;
             }catch(PDOException $e){
                 $link = false;
