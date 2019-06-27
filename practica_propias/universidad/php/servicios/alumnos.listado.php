8edfba43d1b8a0e8e7b7083ba2b86fe810fc17d6
soycolgado <39932051+soycolgado@users.noreply.github.com>
2019-06-27 08:22:02 -0300

    c

---
+++ practica_propias/universidad/php/servicios/alumnos.listado.php
@@ -0,0 +1,14 @@
+<?php
+error_reporting(0);
+
+// Incluir la clase de base de datos
+include_once("../classes/class.Database.php");
+
+// Retorna un json
+header('Content-Type: application/json');
+
+$sql = "SELECT * FROM alumnos ORDER BY nombre ASC";
+
+echo Database::get_json_rows($sql);
+
+?>
