042c449d139b8ad418d43dc254e587740907fb95
soycolgado <soycolgado@gmail.com>
2020-01-23 20:05:06 -0300

    angularya

--- angularya/proyecto004/src/app/app.component.ts
+++ angularya/proyecto004/src/app/app.component.ts
@@ -6,28 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  valor1:number;
-  valor2:number;
-  valor3:number;
-  resultado: string;
+  valor1: number;
+  valor2: number;
+  valor3: number;
+  resultado:string;
+  
   constructor(){
-    this.valor1 = this.retornarAleatorio();
-    this.valor2 = this.retornarAleatorio();
-    this.valor3 = this.retornarAleatorio();
-  }
-
-  retornarAleatorio(){
-    return Math.trunc(Math.random()*6) + 1;
+    this.valor1 = this.retornarRandom();
+    this.valor2 = this.retornarRandom();
+    this.valor3 = this.retornarRandom();
   }
 
   tirar(){
-    this.valor1 = this.retornarAleatorio();
-    this.valor2 = this.retornarAleatorio();
-    this.valor3 = this.retornarAleatorio();
+    this.valor1 = this.retornarRandom();
+    this.valor2 = this.retornarRandom();
+    this.valor3 = this.retornarRandom();
     if(this.valor1 == this.valor2 && this.valor1 == this.valor3){
-      this.resultado = "Has ganado";
+      this.resultado = "Has Ganado!!!!!";
     }else{
-      this.resultado = "Perdiste";
+      this.resultado = "Has Perdido :(";
     }
+  }
+
+  retornarRandom(){
+    let numero = Math.trunc(Math.random()*6) + 1;
+    return numero;
   }
 }
