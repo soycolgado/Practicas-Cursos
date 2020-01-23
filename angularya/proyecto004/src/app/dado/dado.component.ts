4e99a8f5bea51d3e621e46bfb6f34357e5134607
soycolgado <soycolgado@gmail.com>
2020-01-23 13:00:15 -0300

    asd

--- angularya/proyecto004/src/app/dado/dado.component.ts
+++ angularya/proyecto004/src/app/dado/dado.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dado',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dado.component.css']
 })
 export class DadoComponent implements OnInit {
-
+  @Input() valor:number;
   constructor() { }
 
   ngOnInit() {
