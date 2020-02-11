1632c903d220819eb2c03c3a762ded21992457bc
soycolgado <39932051+soycolgado@users.noreply.github.com>
2020-02-11 20:27:28 -0300

    Siguiendo con  Tour

--- angular-tour/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
 import{HeroService} from "../hero.service";
-// import{MessageService} from "../message.service";
+import{MessageService} from "../message.service";
 
 
 @Component({
@@ -12,7 +12,7 @@ import{HeroService} from "../hero.service";
 export class HeroesComponent implements OnInit {
   selectedHero:Hero;
   heroes:Hero[];
-  constructor(private heroService:HeroService) { }
+  constructor(private heroService:HeroService, private messageService:MessageService) { }
 
 
 
@@ -22,10 +22,14 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero:Hero):void{
     this.selectedHero = hero;
+    this.messageService.add(`HeroService: Selected hero id: ${hero.id}`);
   }
 
   getHeroes():void{
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().subscribe(heroes =>{
+      this.heroes = heroes;
+    });
+    
   }
 
 }
