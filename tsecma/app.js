56b2e2ed1e63f896f49650a8da2176a4196ea82a
soycolgado <soycolgado@gmail.com>
2020-02-14 01:47:42 -0300

    Empezando el curso de angular

--- tsecma/app.js
+++ tsecma/app.js
@@ -1,5 +1,22 @@
-const OPCIONES = "todas";
-
-if (true) {
-    const OPCIONES = "ninguna";
+"use strict";
+var nombre = "Ricardo Tapia";
+var edad = 23;
+var PERSONAJE = {
+    nombre: nombre,
+    edad: edad
+};
+var resultadoDoble = function (a, b) { return (a + b) * 2; };
+function getAvenger(nombre, poder, arma) {
+    if (arma === void 0) { arma = "arco"; }
 }
+var Rectangulo = /** @class */ (function () {
+    function Rectangulo(base, altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+    Rectangulo.prototype.calcularArea = function () {
+        this.area = this.base * this.altura;
+        return this.area;
+    };
+    return Rectangulo;
+}());
--- tsecma/app.ts
+++ tsecma/app.ts
@@ -1,19 +1,32 @@
-let nombre:string = "Peter";
-let numero:number = 123;
-let booleano:boolean = true;
-
-let hoy:Date = new Date();
-
-let cualquiera:any;
-cualquiera = nombre;
-cualquiera = numero;
-
-let spiderman = {
-    nombre:"Peter",
-    edad:20
+let nombre:string = "Ricardo Tapia";
+let edad:number = 23;
+const PERSONAJE = {
+    nombre:nombre,
+    edad:edad
 }
 
-spiderman = {
-    nombre: "Juan",
-    edad:20
+interface HEROE{
+    nombre:string;
+    artesMarciales:string[];
+}
+
+let resultadoDoble = (a:number,b:number) => (a+b) * 2;
+
+function getAvenger(nombre:string, poder?:string, arma:string="arco"){
+    
+}
+
+class Rectangulo{
+    base:number;
+    altura:number;
+    area:number;
+    constructor(base:number,altura:number){
+        this.base = base;
+        this.altura = altura;
+    }
+
+    calcularArea():number{
+     this.area = this.base * this.altura;
+     return this.area;
+    }
 }
