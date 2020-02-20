4d769b32c5b6314dcef373906236637520cbe759
soycolgado <soycolgado@gmail.com>
2020-02-20 14:46:19 -0300

    terminando ts

--- tsecma/app.ts
+++ tsecma/app.ts
@@ -1,37 +1,17 @@
 (()=>{
-    // class Avenger{
-    //     nombre:string;
-    //     equipo:string;
-    //     nombreReal:string;
-    //     puedePelear:boolean;
-    //     peleasGanadas:number;
-
-    //     constructor(nombre:string,equipo:string){
-    //         this.nombre = nombre;
-    //         this.equipo = equipo;
-    //     }
-
-        
-    // }
-    
-    class Avenger{
-        // nombre:string;
-        // equipo:string;
-        // nombreReal:string;
-        // puedePelear:boolean;
-        // peleasGanadas:number;
-
-        constructor(public nombre:string,
-                    public equipo:string,
-                    public nombreReal?:string,
-                    public puedePelear:boolean = true,
-                    public peleasGanadas:number = 0){
-            
-        }
-
-        
+    const sumar = (a:number,b:number): number =>{
+        return a + b;
     }
 
-    const Antman:Avenger = new Avenger('AntMan','capi');
-    console.log(Antman);
+    const nombre = ():string => 'Hola Fernando';
+
+    const obtenerSalario = ():Promise<string> =>{
+        return new Promise((resolve,reject)=>{
+            resolve("Nestor Bardel");
+        });
+    }
+
+    obtenerSalario().then(a => console.log(a.toUpperCase));
+    
+
 })();
