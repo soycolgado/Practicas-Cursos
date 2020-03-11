b787211da5275895cb7897297554176a4844f819
soycolgado <soycolgado@gmail.com>
2020-03-10 23:41:42 -0300

    Casi terminando spa

--- AngularUdemy/02-spa/src/app/app.module.ts
+++ AngularUdemy/02-spa/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroeComponent } from './components/heroe/heroe.component';
 import { BuscadorComponent } from './components/buscador/buscador.component';
+import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
 
 
 @NgModule({
@@ -26,7 +27,8 @@ import { BuscadorComponent } from './components/buscador/buscador.component';
     AboutComponent,
     HeroesComponent,
     HeroeComponent,
-    BuscadorComponent
+    BuscadorComponent,
+    HeroeTarjetaComponent
   ],
   imports: [
     BrowserModule,
