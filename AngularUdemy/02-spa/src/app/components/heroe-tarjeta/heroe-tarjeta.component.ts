d0f90f327e653c25323cf55fcdd99465b2eb76d8
soycolgado <soycolgado@gmail.com>
2020-03-11 14:32:14 -0300

    Terminando SPA

--- AngularUdemy/02-spa/src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
+++ AngularUdemy/02-spa/src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
 
 
@@ -13,15 +13,20 @@ export class HeroeTarjetaComponent implements OnInit {
   @Input() heroe: any = {};
   @Input() index: number;
 
+  @Output() heroeSeleccionado: EventEmitter<number>;
 
-  constructor(private router: Router) { }
+
+  constructor(private router: Router) { 
+    this.heroeSeleccionado = new EventEmitter();
+  }
 
   ngOnInit(): void {
     
   }
 
   verHeroe() {
-    this.router.navigate(['/heroe', this.index]);
+    // this.router.navigate(['/heroe', this.index]);
+    this.heroeSeleccionado.emit(this.index);
   }
 
   
