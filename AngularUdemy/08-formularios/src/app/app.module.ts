9b958e62947dcdaf78e7cc986b60d20e95e6378d
soycolgado <soycolgado@gmail.com>
2020-03-27 01:20:26 -0300

    formularios

---
+++ AngularUdemy/08-formularios/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TemplateComponent } from './pages/template/template.component';
+import { ReactiveComponent } from './pages/reactive/reactive.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TemplateComponent,
+    ReactiveComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
